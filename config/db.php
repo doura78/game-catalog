e32d8281167abb03c2aed2d6b2139b13008e766e
sidib <sidibeabdouramane@hotmail.fr>
2026-01-06 09:37:07 +0100

    gitignore

--- config/db.php
+++ config/db.php
@@ -1,13 +1,23 @@
 <?php
 
-return [
+$config = [
     'db' => [
-        'host' => 'mysql',
-        'port' => 3306,
-        'db' => 'game_catalog',
-        'user' => 'app',
-        'pass' => 'app',
-        'charset' => 'utf8mb4',
+        'host' => '',
+        'port' => null,
+        'db' => '',
+        'user' => '',
+        'pass' => '',
+        'charset' => '',
     ]
 ];
+
+$localDbFile = __DIR__ . '/db.local.php';
+//verifier qu'il existe.
+if(is_file($localDbFile)) {
+    // On override db.php par db.local.php
+    $config['db'] = array_replace($config['db'], (require $localDbFile)['db'] ?? []);
+}
+
+// on retourne la bonne config.
+return $config;
 
